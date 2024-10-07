6006622ac499b09691e239f8b68a6e92f6dc46e1
esguerrasolutions <esguerradesign@gmail.com>
2024-10-07 23:10:33 +0800

    arrange react jobs page file initial structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
+import { Route, createBrowserRouter, createRoutesFromElements , RouterProvider } from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path="/" element={<MainLayout/>}>
+    <Route index element={<HomePage/>}/>
+    <Route path="/jobs" element={<JobsPage/>}/>
+    <Route path="*" element={<NotFoundPage/>}/>
+  </Route>
+)
+);
 
 const App = () => {
-  const name = 'John';
-  const names = ['Brad','Mary','Joe'];
-  const loggedIn = false;
-  const styles = {
-    color: 'red',
-    fontSize: '55px',
-  }
-
-  return (
-    <>
-      <div className="text-5xl">App</div>
-      <p style={styles}>Hello {name}</p>
-      <ul>
-        {names.map((name,index)=> (
-          <li key={index}>{name}</li>
-        ))}
-      </ul>
-
-      {loggedIn ? <h1> Hello Member</h1> : <h1>Hello Guess</h1>}
-    </>
-
-  )
+  return <RouterProvider router={router}/>
+  
+  ;
 }
 
-export default App
+export default App;
